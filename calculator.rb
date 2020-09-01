e6b8ddeab70257612d2f04716dfc6f3d651f6f79
Tom ODay <tomoday91@gmail.com>
2020-09-01 16:26:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,5 @@ first_number = 29
 second_number = 11
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
